b1ee1eadf8b0d962074f1f8353e68fe83bec9b4d
med-hamza <mohamedehamza32@gmail.com>
2023-05-01 20:15:29 +0100

    resoudre autoprfier

--- src/api.js
+++ src/api.js
@@ -5,14 +5,7 @@ export async function getHomePage(id) {
     const url = id ?  `/api/${id}` : "/api/"
     const res= await fetch(url)
 
-    if (!res.ok) {
-        throw {
-
-            message: "Failed to fetch campGear",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+    
     const data = await res.json()
     return data.products
 
@@ -24,14 +17,7 @@ export async function getShop(id) {
     const url = id ?  `/api/shop/${id}` : "/api/shop"
     const res= await fetch(url)
 
-    if (!res.ok) {
-        throw {
-
-            message: "Failed to fetch campGear",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+    
     const data = await res.json()
     return data.products
 
@@ -42,14 +28,7 @@ export async function getClimbing(id) {
     const url = id ?  `/api/climbing/${id}` : "/api/climbing"
     const res= await fetch(url)
 
-    if (!res.ok) {
-        throw {
-            
-            message: "Failed to fetch campGear",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+  
     const data = await res.json()
     return data.products
 
@@ -60,14 +39,7 @@ export async function getHiking(id) {
     const url = id ?  `/api/hiking/${id}` : "/api/hiking"
     const res= await fetch(url)
 
-    if (!res.ok) {
-        throw {
-            
-            message: "Failed to fetch campGear",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+
     const data = await res.json()
     return data.products
 
